975b1ba5b36ebb1564f4a407235a65a96d563c5a
hsh0517 <pelino0517@gmail.com>
2023-07-14 11:53:01 +0900

    :hammer: :: ACore 프로젝트 코드 추가

---
+++ ProjectRPG/ACore/Connector.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace ACore
+{
+	public class Connector
+	{
+		private Func<Session> _sessionFactory;
+
+		public void Connect(IPEndPoint endPoint, Func<Session> sessionFactory, int count = 1)
+		{
+			for (int i = 0; i < count; i++)
+			{
+				Socket socket = new Socket(endPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+				socket.NoDelay = true;
+				_sessionFactory = sessionFactory;
+
+				SocketAsyncEventArgs args = new SocketAsyncEventArgs();
+				args.Completed += OnConnectCompleted;
+				args.RemoteEndPoint = endPoint;
+				args.UserToken = socket;
+
+				RegisterConnect(args);
+			}
+		}
+
+		private void RegisterConnect(SocketAsyncEventArgs args)
+		{
+			Socket socket = args.UserToken as Socket;
+			if (socket == null)
+				return;
+
+			try
+			{
+				bool pending = socket.ConnectAsync(args);
+				if (pending == false)
+					OnConnectCompleted(null, args);
+			}
+			catch (Exception e)
+			{
+                Console.WriteLine($"RegisterConnect Failed {e}");
+            }
+		}
+
+		private void OnConnectCompleted(object sender, SocketAsyncEventArgs args)
+		{
+			if (args.SocketError == SocketError.Success)
+			{
+				Session session = _sessionFactory.Invoke();
+				session.Start(args.ConnectSocket);
+				session.OnConnected(args.RemoteEndPoint);
+			}
+			else
+			{
+				Console.WriteLine($"OnConnectCompleted Fail: {args.SocketError}");
+			}
+		}
+	}
+}
---
+++ ProjectRPG/ACore/Listener.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace ACore
+{
+	public class Listener
+	{
+		private Socket _listenSocket;
+		private Func<Session> _sessionFactory;
+
+		public void Init(IPEndPoint endPoint, Func<Session> sessionFactory, int register = 10, int backlog = 100)
+		{
+			_listenSocket = new Socket(endPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+			_listenSocket.NoDelay = true;
+			_sessionFactory += sessionFactory;
+
+			_listenSocket.Bind(endPoint);
+			_listenSocket.Listen(backlog);
+
+			for (int i = 0; i < register; i++)
+			{
+				SocketAsyncEventArgs args = new SocketAsyncEventArgs();
+				args.Completed += new EventHandler<SocketAsyncEventArgs>(OnAcceptCompleted);
+				RegisterAccept(args);
+			}
+		}
+
+		private void RegisterAccept(SocketAsyncEventArgs args)
+		{
+			args.AcceptSocket = null;
+
+			try
+			{
+				bool pending = _listenSocket.AcceptAsync(args);
+				if (pending == false)
+					OnAcceptCompleted(null, args);
+			}
+			catch (Exception e)
+			{
+                Console.WriteLine($"RegisterAccept Failed {e}");
+            }
+		}
+
+		private void OnAcceptCompleted(object sender, SocketAsyncEventArgs args)
+		{
+			if (args.SocketError == SocketError.Success)
+			{
+				Session session = _sessionFactory.Invoke();
+				session.Start(args.AcceptSocket);
+				session.OnConnected(args.AcceptSocket.RemoteEndPoint);
+			}
+			else
+			{
+				Console.WriteLine($"OnAcceptCompleted Failed {args.SocketError}");
+			}
+
+			RegisterAccept(args);
+		}
+	}
+}
