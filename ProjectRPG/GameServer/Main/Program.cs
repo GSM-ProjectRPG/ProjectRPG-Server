1576cfde90bb8ecfbed5b8206a0e642d48ce6bd4
hsh0517 <pelino0517@gmail.com>
2023-07-14 12:09:29 +0900

    :hammer: :: Program 클래스 생성

---
+++ ProjectRPG/GameServer/Main/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectRPG
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            ServerService server = new ServerService();
+
+            server.Start();
+
+            while (true)
+            {
+
+            }
+        }
+    }
+}
