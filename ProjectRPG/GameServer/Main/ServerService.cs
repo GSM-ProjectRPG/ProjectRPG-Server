1cc8caba822c1d965197b605c69ff167950bde1d
hsh0517 <pelino0517@gmail.com>
2023-07-14 12:08:41 +0900

    :hammer: :: ServerService 클래스 생성

---
+++ ProjectRPG/GameServer/Main/ServerService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Net;
+using ACore;
+
+namespace ProjectRPG
+{
+    public class ServerService
+    {
+        private Listener _listener = new Listener();
+
+        public void Start()
+        {
+            string host = Dns.GetHostName();
+            IPHostEntry ipHost = Dns.GetHostEntry(host);
+            IPAddress ipAddr = ipHost.AddressList[0];
+            IPEndPoint endPoint = new IPEndPoint(ipAddr, 9999);
+
+            //_listener.Init(endPoint, SessionManager.Instance.Generate);
+            Console.WriteLine("Server is running...");
+        }
+    }
+}
