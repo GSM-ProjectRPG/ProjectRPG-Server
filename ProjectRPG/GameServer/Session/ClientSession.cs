70df05a1e94cacdc8b8a5b16864ff347210a1e9b
hsh0517 <pelino0517@gmail.com>
2023-07-14 12:12:59 +0900

    :hammer: :: ClientSession 클래스 생성

---
+++ ProjectRPG/GameServer/Session/ClientSession.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net;
+using ACore;
+
+namespace ProjectRPG.Session
+{
+    public class ClientSession : PacketSession
+    {
+        public int SessionId { get; set; }
+
+        public override void OnConnected(EndPoint endPoint)
+        {
+
+        }
+
+        public override void OnDisconnected(EndPoint endPoint)
+        {
+
+        }
+
+        public override void OnRecvPacket(ArraySegment<byte> buffer)
+        {
+
+        }
+
+        public override void OnSend(int numOfBytes)
+        {
+
+        }
+    }
+}
