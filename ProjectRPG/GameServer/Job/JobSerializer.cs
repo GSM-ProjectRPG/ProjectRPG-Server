bacb9c68f97fbb9c9a6a7c0947eb1a58502d8c12
hsh0517 <pelino0517@gmail.com>
2023-08-23 17:18:22 +0900

    :sparkles: :: Job 기능 추가

---
+++ ProjectRPG/GameServer/Job/JobSerializer.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GameServer.Job
+{
+    public class JobSerializer
+    {
+        private JobTimer _jobTimer = new JobTimer();
+        private Queue<BaseJob> _jobQueue = new Queue<BaseJob>();
+        private readonly object _lock = new object();
+
+        public BaseJob PushAfter(int tickAfter, Action action) => PushAfter(tickAfter, new Job(action));
+        public BaseJob PushAfter<T1>(int tickAfter, Action<T1> action, T1 t1) => PushAfter(tickAfter, new Job<T1>(action, t1));
+        public BaseJob PushAfter<T1, T2>(int tickAfter, Action<T1, T2> action, T1 t1, T2 t2) => PushAfter(tickAfter, new Job<T1, T2>(action, t1, t2));
+        public BaseJob PushAfter<T1, T2, T3>(int tickAfter, Action<T1, T2, T3> action, T1 t1, T2 t2, T3 t3) => PushAfter(tickAfter, new Job<T1, T2, T3>(action, t1, t2, t3));
+
+        public BaseJob PushAfter(int tickAfter, BaseJob job)
+        {
+            _jobTimer.Push(job, tickAfter);
+            return job;
+        }
+
+        public void Push(Action action) => Push(new Job(action));
+        public void Push<T1>(Action<T1> action, T1 t1) => Push(new Job<T1>(action, t1));
+        public void Push<T1, T2>(Action<T1, T2> action, T1 t1, T2 t2) => Push(new Job<T1, T2>(action, t1, t2));
+        public void Push<T1, T2, T3>(Action<T1, T2, T3> action, T1 t1, T2 t2, T3 t3) => Push(new Job<T1, T2, T3>(action, t1, t2, t3));
+
+        public void Push(BaseJob job)
+        {
+            lock (_lock)
+            {
+                _jobQueue.Enqueue(job);
+            }
+        }
+
+        public void Flush()
+        {
+            _jobTimer.Flush();
+
+            while (true)
+            {
+                BaseJob job = Pop();
+                if (job == null) return;
+                job.Execute();
+            }
+        }
+
+        private BaseJob Pop()
+        {
+            lock (_lock)
+            {
+                if (_jobQueue.Count == 0) return null;
+                return _jobQueue.Dequeue();
+            }
+        }
+    }
+}
---
+++ ProjectRPG/GameServer/Job/JobTimer.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using ACore;
+
+namespace GameServer.Job
+{
+    struct JobTimerElem : IComparable<JobTimerElem>
+    {
+        public int execTick;
+        public BaseJob Job;
+
+        public int CompareTo(JobTimerElem other)
+        {
+            return other.execTick - execTick;
+        }
+    }
+
+    public class JobTimer
+    {
+        private PriorityQueue<JobTimerElem> _pq = new PriorityQueue<JobTimerElem>();
+        private readonly object _lock = new object();
+
+        public void Push(BaseJob job, int tickAfter = 0)
+        {
+            JobTimerElem jobElement;
+            jobElement.execTick = Environment.TickCount + tickAfter;
+            jobElement.Job = job;
+
+            lock (_lock)
+            {
+                _pq.Push(jobElement);
+            }
+        }
+
+        public void Flush()
+        {
+            while (true)
+            {
+                int now = Environment.TickCount;
+
+                JobTimerElem jobElement;
+
+                lock (_lock)
+                {
+                    if (_pq.Count == 0)
+                        break;
+
+                    jobElement = _pq.Peek();
+                    if (jobElement.execTick > now)
+                        break;
+
+                    _pq.Pop();
+                }
+
+                jobElement.Job.Execute();
+            }
+        }
+    }
+}
