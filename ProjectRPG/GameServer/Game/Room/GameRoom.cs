77f8a2d73276136f861aebbc26c544d283d078dd
hsh0517 <pelino0517@gmail.com>
2023-08-23 17:34:51 +0900

    :sparkles: :: Room 관련 클래스 추가

---
+++ ProjectRPG/GameServer/Game/Room/GameRoom.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProjectRPG.Job;
+
+namespace ProjectRPG.Game
+{
+    public partial class GameRoom : JobSerializer
+    {
+        public int RoomId { get; set; }
+
+        public void Update()
+        {
+            Flush();
+        }
+    }
+}
---
+++ ProjectRPG/GameServer/Game/Room/GameRoom_Item.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProjectRPG.Job;
+
+namespace ProjectRPG.Game
+{
+    public partial class GameRoom : JobSerializer
+    {
+
+    }
+}
