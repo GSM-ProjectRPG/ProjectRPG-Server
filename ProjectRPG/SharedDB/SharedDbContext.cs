c297f9317c44f17c828cb14afd327bc69ee4338c
hsh0517 <pelino0517@gmail.com>
2023-08-24 18:06:53 +0900

    :sparkles: :: SharedDbContext 기능 추가

--- ProjectRPG/SharedDB/SharedDbContext.cs
+++ ProjectRPG/SharedDB/SharedDbContext.cs
@@ -1,13 +1,46 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.EntityFrameworkCore;
 
 namespace SharedDB
 {
-    public class SharedDbContext
+    public class SharedDbContext : DbContext
     {
+        public DbSet<TokenDb> Tokens { get; set; }
+        public DbSet<ServerDb> Servers { get; set; }
 
+        // GameServer
+        public SharedDbContext()
+        {
+
+        }
+
+        // AccountServer
+        public SharedDbContext(DbContextOptions<SharedDbContext> options) : base(options)
+        {
+
+        }
+
+        // GameServer
+        public static string ConnectionString { get; set; } = @"Data Source=(localdb)\ProjectModels;Initial Catalog=ProjectRPG_SharedDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+        
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            if (options.IsConfigured == false)
+            {
+                options
+                //.UseLoggerFactory(_logger)
+                .UseSqlServer(ConnectionString);
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<TokenDb>()
+                .HasIndex(t => t.AccountDbId)
+                .IsUnique();
+
+            builder.Entity<ServerDb>()
+                .HasIndex(s => s.Name)
+                .IsUnique();
+        }
     }
 }
