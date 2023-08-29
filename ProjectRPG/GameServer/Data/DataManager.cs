0fef7444f24cd5e2f0f4f8f07821992c292b8b41
hsh0517 <pelino0517@gmail.com>
2023-08-30 08:46:48 +0900

    :sparkles: :: Skill 및 Item 관련 Protocol 추가

--- ProjectRPG/GameServer/Data/DataManager.cs
+++ ProjectRPG/GameServer/Data/DataManager.cs
@@ -1,5 +1,6 @@
 ﻿using System.IO;
 using System.Collections.Generic;
+using Google.Protobuf.Protocol;
 
 namespace ProjectRPG.Data
 {
@@ -10,18 +11,18 @@ namespace ProjectRPG.Data
 
     public class DataManager
     {
-        //public static Dictionary<int, StatInfo> StatDict { get; private set; } = new Dictionary<int, StatInfo>();
-        //public static Dictionary<int, Data.Skill> SkillDict { get; private set; } = new Dictionary<int, Data.Skill>();
-        //public static Dictionary<int, Data.ItemData> ItemDict { get; private set; } = new Dictionary<int, Data.ItemData>();
-        //public static Dictionary<int, Data.MonsterData> MonsterDict { get; private set; } = new Dictionary<int, Data.MonsterData>();
+        public static Dictionary<int, StatInfo> StatDict { get; private set; } = new Dictionary<int, StatInfo>();
+        public static Dictionary<int, Data.Skill> SkillDict { get; private set; } = new Dictionary<int, Data.Skill>();
+        public static Dictionary<int, Data.ItemData> ItemDict { get; private set; } = new Dictionary<int, Data.ItemData>();
+        public static Dictionary<int, Data.MonsterData> MonsterDict { get; private set; } = new Dictionary<int, Data.MonsterData>();
 
-        //public static void LoadData()
-        //{
-        //    StatDict = LoadJson<Data.StatData, int, StatInfo>("StatData").MakeDict();
-        //    SkillDict = LoadJson<Data.SkillData, int, Data.Skill>("SkillData").MakeDict();
-        //    ItemDict = LoadJson<Data.ItemLoader, int, Data.ItemData>("ItemData").MakeDict();
-        //    MonsterDict = LoadJson<Data.MonsterLoader, int, Data.MonsterData>("MonsterData").MakeDict();
-        //}
+        public static void LoadData()
+        {
+            StatDict = LoadJson<Data.StatData, int, StatInfo>("StatData").MakeDict();
+            SkillDict = LoadJson<Data.SkillData, int, Data.Skill>("SkillData").MakeDict();
+            ItemDict = LoadJson<Data.ItemLoader, int, Data.ItemData>("ItemData").MakeDict();
+            MonsterDict = LoadJson<Data.MonsterLoader, int, Data.MonsterData>("MonsterData").MakeDict();
+        }
 
         private static Loader LoadJson<Loader, Key, Value>(string path) where Loader : ILoader<Key, Value>
         {
