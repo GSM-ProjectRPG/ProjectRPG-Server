65d7bfc6d88c81e33bcba9b617bc97928980dfb3
hsh0517 <pelino0517@gmail.com>
2023-08-29 16:08:41 +0900

    :sparkles: :: AppDbContext 오버라이드 메서드 추가

--- ProjectRPG/GameServer/DB/AppDbContext.cs
+++ ProjectRPG/GameServer/DB/AppDbContext.cs
@@ -1,14 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 
 namespace ProjectRPG.DB
 {
     public class AppDbContext : DbContext
     {
+        public DbSet<AccountDb> Accounts { get; set; }
+        public DbSet<PlayerDb> Players { get; set; }
+        public DbSet<ItemDb> Items { get; set; }
 
+        private static readonly ILoggerFactory _logger = LoggerFactory.Create(builder => builder.AddConsole());
+
+        private string _connectionString = @"";
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //optionsBuilder
+            //    .UseLoggerFactory(_logger)
+            //    .UseSqlServer(ConfigManager.Config == null ? _connectionString : ConfigManager.Config.connectionString);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<AccountDb>()
+                .HasIndex(a => a.AccountName)
+                .IsUnique();
+
+            modelBuilder.Entity<PlayerDb>()
+                .HasIndex(p => p.PlayerName)
+                .IsUnique();
+        }
     }
 }
