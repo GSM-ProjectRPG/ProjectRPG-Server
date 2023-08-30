f95687aa1a26eb350e0e3f726d8a1a977fb38fda
hsh0517 <pelino0517@gmail.com>
2023-08-30 09:39:40 +0900

    :sparkles: :: 폴더 이름 변경 및 EntityManager 추가

---
+++ ProjectRPG/GameServer/Game/Entity/EntityManager.cs
@@ -0,0 +1,38 @@
+﻿using Google.Protobuf.Protocol;
+
+namespace ProjectRPG.Game
+{
+    public class EntityManager
+    {
+        public static EntityManager Instance { get; } = new EntityManager();
+
+        private readonly object _lock = new object();
+        private int _count = 0;
+
+        public T Add<T>() where T : Entity, new()
+        {
+            var entity = new T();
+
+            lock (_lock)
+            {
+                entity.Id = GenerateId(entity.Type);
+            }
+
+            return entity;
+        }
+
+        private int GenerateId(EntityType type)
+        {
+            lock (_lock)
+            {
+                return ((int)type << 24) | (_count++);
+            }
+        }
+
+        public static EntityType GetEntityTypeById(int id)
+        {
+            int type = (id >> 24) & 0x7F;
+            return (EntityType)type;
+        }
+    }
+}
