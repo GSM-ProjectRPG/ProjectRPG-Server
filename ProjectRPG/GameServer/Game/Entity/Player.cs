41ba73dfab759a33621fb48687cd601c8f8f3a4c
hsh0517 <pelino0517@gmail.com>
2023-08-30 09:45:13 +0900

    :sparkles: :: Player 추가

---
+++ ProjectRPG/GameServer/Game/Entity/Player.cs
@@ -0,0 +1,20 @@
+﻿using Google.Protobuf.Protocol;
+
+namespace ProjectRPG.Game
+{
+    public class Player : Entity
+    {
+        public int PlayerDbId { get; set; }
+        public ClientSession Session { get; set; }
+
+        public Player()
+        {
+            Type = EntityType.Player;
+        }
+
+        public override void OnDead(Entity killer)
+        {
+            base.OnDead(killer);
+        }
+    }
+}
