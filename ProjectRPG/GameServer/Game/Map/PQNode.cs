3f1e18723ee4bcee9141570c80a909f0ac3d42cd
hsh0517 <pelino0517@gmail.com>
2023-09-06 17:16:41 +0900

    :sparkles: :: 길찾기 알고리즘 관련 구조체 추가

---
+++ ProjectRPG/GameServer/Game/Map/PQNode.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ProjectRPG.Game
+{
+    public struct PQNode : IComparable<PQNode>
+    {
+        public int F;
+        public int G;
+        public int Y;
+        public int X;
+
+        public int CompareTo(PQNode other)
+        {
+            if (F == other.F)
+                return 0;
+
+            return F < other.F ? 1 : -1;
+        }
+    }
+}
---
+++ ProjectRPG/GameServer/Game/Map/Pos.cs
@@ -0,0 +1,33 @@
+﻿namespace ProjectRPG.Game
+{
+    public struct Pos
+    {
+        public int Y;
+        public int X;
+
+        public Pos(int y, int x)
+        {
+            Y = y;
+            X = x;
+        }
+
+        public static bool operator==(Pos lhs, Pos rhs) => lhs.Y == rhs.Y && lhs.X == rhs.X;
+        public static bool operator!=(Pos lhs, Pos rhs) => !(lhs == rhs);
+
+        public override bool Equals(object obj)
+        {
+            return (Pos)obj == this;
+        }
+
+        public override int GetHashCode()
+        {
+            long value = (Y << 32) | X;
+            return value.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
