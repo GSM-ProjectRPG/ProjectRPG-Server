8ca96ae13ba52a12efcb8f63f43d2c7849ba564e
hsh0517 <pelino0517@gmail.com>
2023-09-07 15:39:04 +0900

    :sparkles: :: Player에 VisionField 추가

--- ProjectRPG/GameServer/Game/Object/Player.cs
+++ ProjectRPG/GameServer/Game/Object/Player.cs
@@ -6,12 +6,14 @@ namespace ProjectRPG.Game
     {
         public int PlayerDbId { get; set; }
         public ClientSession Session { get; set; }
+        public VisionField Vision { get; private set; }
 
         public Inventory Inven { get; set; }
 
         public Player()
         {
             Type = GameObjectType.Player;
+            Vision = new VisionField(this);
         }
 
         public override void OnDead(GameObject killer)
