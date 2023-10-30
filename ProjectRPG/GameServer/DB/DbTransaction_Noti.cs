4e05265f6ec80f77ae3e0469513245d163e8f2d4
hsh0517 <pelino0517@gmail.com>
2023-10-30 09:35:36 +0900

    :sparkles: :: 아이템 지급 DB 로직 추가

--- ProjectRPG/GameServer/DB/DbTransaction_Noti.cs
+++ ProjectRPG/GameServer/DB/DbTransaction_Noti.cs
@@ -1,9 +1,10 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using GameServer.Game;
+using GameServer.Job;
 
 namespace GameServer.DB
 {
-    public partial class DbTransaction
+    public partial class DbTransaction : JobSerializer
     {
         
     }
